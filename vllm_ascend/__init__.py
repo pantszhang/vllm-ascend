01c6f4f64c0a67fd2c7e05672aa998ce1528f8ce
pantszhang <601561479@qq.com>
2026-07-21 19:22:51 +0800

    Revert "feat(ec): register ECMemCacheConnector in plugin entry"
    This reverts commit f58b90da60758aa3ce4d2dc8458bf5bc6da4abc1.

--- vllm_ascend/__init__.py
+++ vllm_ascend/__init__.py
@@ -46,11 +46,9 @@ def register_connector():
 
     from vllm_ascend.distributed.kv_transfer import register_connector
     from vllm_ascend.distributed.weight_transfer import register_engine
-    from vllm_ascend.distributed import register_ec_connector
 
     register_connector()
     register_engine()
-    register_ec_connector()
 
 
 def register_model_loader():
